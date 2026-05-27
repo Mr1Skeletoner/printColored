bd27a2f9cdc011de96027e8c4a22db6f690d6481
Mr1Skeletoner <glitchtrapissus@gmail.com>
2026-05-28 01:11:26 +0300

    Update printColored.py

--- printColored.py
+++ printColored.py
@@ -40,7 +40,7 @@ def printColored(endl, text, *format,):
     if endl == "form dict" or text == "form dict" or format=="form dict":
         for key, value in formats.items():
            print(f"\033[{value}m{key}\033[0m")
-    elif format not in format:
+    elif format not in formats:
         print("The format couldnt be recognized")
     else:
         format_code = ";".join(formats[name] for name in format)
